ed8641710a15e4e08482bcaa34f998b2aa797957
goatps <pranav.soni@puresoftware.com>
2023-03-16 10:35:47 +0530

    Added todos page

---
+++ src/Todo/Todos.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+
+const Todos = () => {
+    return(
+        <>
+            <Heading>
+                Hi this is todos
+            </Heading>
+        </>
+    )
+}
+
+export default Todos;
