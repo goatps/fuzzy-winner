5397492349931c5ea194d825e94cb529a4a84a07
goatps <pranav.soni@puresoftware.com>
2023-03-16 14:17:59 +0530

    Defined layout of todos page

--- src/Todo/Todos.tsx
+++ src/Todo/Todos.tsx
@@ -1,12 +1,162 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
-
+import { Box, Button, Grid, GridItem, 
+         Heading, IconButton, Text, 
+         useColorModeValue 
+} from '@chakra-ui/react';
+import { CheckIcon, ArrowRightIcon } from "@chakra-ui/icons";
 const Todos = () => {
     return(
         <>
             <Heading>
                 Hi this is todos
             </Heading>
+            <Grid
+                p= "1em 2em" 
+                display = "flex"
+                justifyContent= "center"
+                templateColumns = "repeat(12, 1fr)"
+                gap={4}
+            >
+                <GridItem
+                    colSpan={3}
+                    width = "100%"
+                >
+                    <Box
+                        p ="1em"
+                        // bg = "cyan"
+                        // color= "gray.700"
+                        bg={useColorModeValue("teal.400", "teal")}
+                        borderRadius= "lg"
+                    >
+                        <Heading
+                            as= "h3"
+                            size = "sm"
+                            textAlign = "center"
+                            textTransform= "uppercase"
+                        >
+                            Created Task
+                        </Heading>
+                        <Box
+                            p = "1em"
+                            mt = "1em"
+                            bg={useColorModeValue("white", "gray.700")}
+                            borderRadius= "lg"
+                            // color = "black"
+                        >
+                            <Heading
+                                as = "h5"
+                                size = "xs"
+                            >
+                                Task heading
+                            </Heading>
+                            <Text
+                                color= {useColorModeValue("gray", "whiteAlpha.500")}
+                            >
+                                demo task
+                            </Text>
+                            <IconButton
+                                mt = "0.5em" 
+                                colorScheme= "red"
+                                color = {useColorModeValue("white", "black")}
+                                bg={useColorModeValue("red.400", "red.500")}
+                                aria-label='task completed icon' 
+                                icon={<ArrowRightIcon />
+                            }
+                            />
+                        </Box>
+                    </Box>
+                </GridItem>
+                <GridItem
+                    width = "100%"                    
+                    colSpan={3}
+                >
+                    <Box
+                        p ="1em"
+                        bg={useColorModeValue("red.400", "red.500")}
+                        borderRadius= "lg"
+                    >
+                        <Heading
+                            as= "h3"
+                            size = "sm"
+                            textAlign = "center"
+                            textTransform= "uppercase"
+                        >
+                            Ongoing Task
+                        </Heading>
+                        <Box
+                            p = "1em"
+                            mt = "1em"
+                            bg={useColorModeValue("white", "gray.700")}
+                            borderRadius= "lg"
+                            // color = "black"
+                        >
+                            <Heading
+                                as = "h5"
+                                size = "xs"
+                            >
+                                Task heading
+                            </Heading>
+                            <Text
+                                color= {useColorModeValue("gray", "whiteAlpha.500")}
+                            >
+                                demo task
+                            </Text>
+                            <IconButton
+                                mt = "0.5em" 
+                                colorScheme= "green"
+                                aria-label='task completed icon' 
+                                icon={<CheckIcon />}
+                            />
+                        </Box>
+                    </Box>
+                </GridItem>
+                <GridItem
+                    colSpan={3}
+                    width = "100%"
+                    >
+                    <Box
+                        p ="1em"
+                        bg={useColorModeValue("green.500", "green.700")}
+                        borderRadius= "lg"
+                    >
+                        <Heading
+                            as= "h3"
+                            size = "sm"
+                            textAlign = "center"
+                            textTransform= "uppercase"
+                        >
+                            Completed Task
+                        </Heading>
+                        <Box
+                            p = "1em"
+                            mt = "1em"
+                            bg={useColorModeValue("white", "gray.700")}
+                            borderRadius= "lg"
+                            // color = "black"
+                        >
+                            <Heading
+                                as = "h5"
+                                size = "xs"
+                            >
+                                Task heading
+                            </Heading>
+                            <Text
+                                color= {useColorModeValue("gray", "whiteAlpha.500")}
+                            >
+                                demo task
+                            </Text>
+                            <Button
+                                mt = "0.5em"
+                                colorScheme="green"
+                                isDisabled
+                            >
+                                Task Completed
+                            </Button>
+                        </Box>
+                    </Box>
+                </GridItem>
+                
+            </Grid>
         </>
     )
 }
